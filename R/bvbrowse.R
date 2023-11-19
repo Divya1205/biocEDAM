b32fdc8a04bb0adeb5739227a8fb792c593687e4
vjcitn <stvjc@channing.harvard.edu>
2023-11-18 21:24:34 -0500

    update app

--- R/bvbrowse.R
+++ R/bvbrowse.R
@@ -2,56 +2,77 @@
 #' @import shiny
 #' @import graph
 #' @import BiocPkgTools
-#' @param \dots passed to biocPkgList
 #' @export
-bvbrowse = function(...) {
+bvbrowse = function() {
 # will only work for software in this state
 # library(biocViews)
 # library(shiny)
 # library(graph)
 # library(BiocPkgTools)
 
-# build a mapping from views to package names
- pl = biocPkgList(...)  # tibble, biocViews field is a list
- vl = pl$biocViews
- vls = vapply(vl, length, numeric(1))
- ps = rep(pl$Package, vls)
- vdf = data.frame(view = unlist(vl), pkg = ps)
 #
 
- data(biocViewsVocab)
+ data("biocViewsVocab", package="biocViews")
  bv = biocViewsVocab
- el = edgeL(bv)
- nv = nodes(bv)
+ el = graph::edgeL(bv)
+ nv = graph::nodes(bv)
+
+toptags = c(BioCsoft = "Software", 
+  BioCann = "AnnotationData", BioCexp = "ExperimentData", 
+  BioCworkflows = "Workflow")
+
 
  ui = fluidPage(
  sidebarLayout(
   sidebarPanel(
    helpText("biocViews browser"),
-   radioButtons("top", "top", nv[el[[1]]$edges]), 
-   uiOutput("lev2"),
+   radioButtons("type", "Repo", c("BioCsoft", "BioCann",
+     "BioCexp", "BioCworkflows"), selected="BioCsoft"),
+   uiOutput("selbut"),
    width=2
    ),
   mainPanel(
    tabsetPanel(
-    tabPanel("sub", uiOutput("lev3"), DT::dataTableOutput("sub1"))
+    tabPanel("Packages", uiOutput("lev3"), DT::dataTableOutput("sub1"))
     )
    )
   )
  )
 
  server = function(input, output) {
+  setupDF = reactive({
+  # build a mapping from views to package names
+   pl = BiocPkgTools::biocPkgList(repo=input$type)  # tibble, biocViews field is a list
+   pl$tags = sapply(pl$biocViews, paste, collapse=":")
+   vl = pl$biocViews
+   vls = vapply(vl, length, numeric(1))
+   ps = rep(pl$Package, vls)
+   vdf = data.frame(view = unlist(vl), pkg = ps)
+   list(vdf=vdf, pl=pl)
+   })
+  output$selbut = renderUI({
+   validate(need(length(input$type)>0, "waiting"))
+   radioButtons("top", "RepoCategory", nv[el[[toptags[input$type]]]$edges])
+   })
+
   output$sub1 = DT::renderDataTable({
-  pks = unique(vdf[which(vdf$view %in% input$lev3),]$pkg)
+  validate(need(length(input$lev2)>0, "preparing data table"))
+  vdfpl = setupDF()
+  vdf = vdfpl$vdf
+  pks = unique(vdf[which(vdf$view %in% input$lev2),]$pkg)
+  pl = vdfpl$pl
   ans = pl[which(pl$Package %in%  pks),]
-  as.data.frame(ans[, c("Package", "Version", "dependencyCount")])
+  validate(need(nrow(ans)>0, "no packages, pick another category"))
+  as.data.frame(ans[, c("Package", "Version", "dependencyCount", "tags")])
   })
-  output$lev2 = renderUI({
-   radioButtons("lev2", "lev2", nv[el[[ input$top ]]$edges])
-  })
+#  output$lev2 = renderUI({
+#   radioButtons("lev2", "lev2", nv[el[[ input$top ]]$edges])
+#  })
   output$lev3 = renderUI({
-   checkboxGroupInput("lev3", "lev3", nv[el[[ input$lev2 ]]$edges], 
-     inline=TRUE, selected = nv[el[[ input$lev2 ]]$edges][1:4])
+   validate(need(length(input$top)>0, "waiting"))
+   tmp = nv[el[[ input$top ]]$edges]
+   checkboxGroupInput("lev2", input$top, tmp,
+     inline=TRUE, selected = tmp[seq_len(min(c(length(tmp),4)))])
   })
 }
 
